32f48c26b857267070e07689979cfe1d70806a66
Felipe Reigosa <felipereigosa@gmail.com>
2023-06-19 16:13:30 -0300

    Created code blocks tabs

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kairon
+
+Kairon is a VR code editor. The idea is you never have to take the headset off but you interact with the code you see inside it with the keyboard on your computer. You can also interact with the world you generate dinamically with the headset controllers.
+
+[![Introductory video](https://mockmechanics.com/mechanics/thumbnail-yt.png)](https://www.youtube.com/watch?v=HrwxbQj5mj0)
+
+# How to run
+
+...
--- src/editor.js
+++ src/editor.js
@@ -2,38 +2,29 @@ import * as THREE from 'three';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { highlight } from './highlight';
 
+const textOffset = [-2, 2];
+
 export class Editor {
   constructor(scene) {
-    this.mesh = this.createPlane(7, 5, 0.2);
+    this.mesh = this.createPlane(9, 5, 0.2);
     this.codeBlocks = [{text: "const x = 10;\nconsole.log('value = ' + x);",
-                        index: 5},
-                       {text: "const second = 2;",
-                        index: 0},
-                       {text: "const third = 3;",
-                        index: 0},
-                      ];
-    this.codeBlock = this.codeBlocks[0];
-
-    this.cursor = new THREE.Mesh(new THREE.PlaneGeometry(1, 1),
-                                 new THREE.MeshBasicMaterial({color: 0xaaaaaa}));
-    this.cursor.position.set(-2.92, 2.1, 0.01);
-    this.cursor.scale.set(0.15, 0.3, 1);
+                        index: 0}];
+    this.tab = 0;
+    this.cursor = new THREE.Mesh(new THREE.PlaneGeometry(0.15, 0.3),
+                                 new THREE.MeshBasicMaterial({color: 0x101010}));
+    this.cursor.position.set(textOffset[0] + 0.08, textOffset[1] + 0.1, 0.01);
     this.mesh.add(this.cursor);
 
     const fontLoader = new FontLoader();
     fontLoader.load('/droid_sans_mono_regular.typeface.json', (font) => {
-      const material = new THREE.MeshBasicMaterial( {
-        color: 0,
-        transparent: true,
-        opacity: 0.8,
-      });
-
       this.font = font;
       const shapes = font.generateShapes("", 1);
       const geometry = new THREE.ShapeGeometry(shapes);
+      const material = new THREE.MeshBasicMaterial({transparent: true,
+                                                    opacity: 0.8});
       const template = new THREE.Mesh(geometry, material);
       template.scale.set(0.2, 0.2, 0.2);
-      template.position.set(-3, 2, 0.02);
+      template.position.set(textOffset[0], textOffset[1], 0.02);
 
       const colors = {"red": 0xff0020,
                       "green": 0x20ff20,
@@ -55,8 +46,27 @@ export class Editor {
         this.mesh.add(mesh);
       }
 
+      this.tabSelector = new THREE.Mesh(new THREE.PlaneGeometry(1.8, 0.3),
+                                        new THREE.MeshBasicMaterial({color: 0x101010}));
+      this.tabSelector.position.set(-3.3, 2.06 - 0.3, 0.01);
+      this.mesh.add(this.tabSelector);
+
+      const tabShapes = font.generateShapes("createSphe...\nbar\nsomething", 0.8);
+      this.tabText = new THREE.Mesh(new THREE.ShapeGeometry(tabShapes),
+                                    new THREE.MeshBasicMaterial({color: 0xaaaaaa}));
+      this.tabText.scale.set(0.2, 0.2, 0.2);
+      this.tabText.position.set(textOffset[0] - 2.2, textOffset[1], 0.02);
+      this.mesh.add(this.tabText);
+
       this.update();
     });
+
+    const divider = new THREE.Mesh(new THREE.PlaneGeometry(1, 1),
+                                   new THREE.MeshBasicMaterial({color: 0xaaaaaa}));
+    divider.position.set(textOffset[0] - 0.3, 0, 0.01);
+    divider.scale.set(0.01, 4.5, 1);
+
+    this.mesh.add(divider);
   }
 
   createPlane (width, height, radius) {
@@ -90,13 +100,15 @@ export class Editor {
   }
 
   getTotal (row) {
-    const lines = this.codeBlock.text.split("\n");
+    const block = this.codeBlocks[this.tab];
+    const lines = block.text.split("\n");
     return lines.slice(0, row).reduce((acc, x) => acc + x.length + 1, 0);
   }
 
   getRowCol () {
-    const index = this.codeBlock.index;
-    const before = this.codeBlock.text.substring(0, index);
+    const block = this.codeBlocks[this.tab];
+    const index = block.index;
+    const before = block.text.substring(0, index);
     const lines = before.split("\n");
     const row = lines.length - 1;
     const col = index - this.getTotal(row);
@@ -104,13 +116,15 @@ export class Editor {
   }
 
   setRowCol (row, col) {
-    const lines = this.codeBlock.text.split("\n");
+    const block = this.codeBlocks[this.tab];
+    const lines = block.text.split("\n");
     col = Math.min(lines[row].length, col);
-    this.codeBlock.index = this.getTotal(row) + col;
+    block.index = this.getTotal(row) + col;
   }
 
   update () {
-    const result = highlight(this.codeBlock.text);
+    const block = this.codeBlocks[this.tab];
+    const result = highlight(block.text);
 
     for (let color of Object.keys(result)) {
       const colorMesh = this.mesh.getObjectByName(color);
@@ -120,30 +134,43 @@ export class Editor {
     }
 
     const [row, col] = this.getRowCol();
-    this.cursor.position.set(-2.92 + 0.167 * col,
-                             2.1 - 0.39 * row,
+    this.cursor.position.set(textOffset[0] + 0.08 + 0.167 * col,
+                             textOffset[1] + 0.1 - 0.39 * row,
                              0.01);
+
+    this.tabSelector.position.set(-3.3, 2.06 - 0.3 * this.tab, 0.01);
+
+    this.tabText.geometry.dispose();
+
+    let text = "";
+    for (let i = 0; i < this.codeBlocks.length; i++) {
+      text += "" + i + "\n";
+    }
+    const shapes = this.font.generateShapes(text, 0.8);
+    this.tabText.geometry = new THREE.ShapeGeometry(shapes);
   }
 
   splitText () {
-    const before = this.codeBlock.text.substring(0, this.codeBlock.index);
-    const after = this.codeBlock.text.substring(this.codeBlock.index);
+    const block = this.codeBlocks[this.tab];
+    const before = block.text.substring(0, block.index);
+    const after = block.text.substring(block.index);
     return [before, after];
   }
 
   insert (key) {
     const [before, after] = this.splitText();
-    this.codeBlock.text = before + key + after;
-    this.codeBlock.index += 1;
+    const block = this.codeBlocks[this.tab];
+    block.text = before + key + after;
+    block.index += 1;
     this.update();
   }
 
   moveCursor (x, y) {
+    const block = this.codeBlocks[this.tab];
     if (x < 0) {
-      this.codeBlock.index = Math.max(0, this.codeBlock.index - 1);
+      block.index = Math.max(0, block.index - 1);
     } else if (x > 0) {
-      this.codeBlock.index = Math.min(this.codeBlock.index + 1,
-                                      this.codeBlock.text.length);
+      block.index = Math.min(block.index + 1, block.text.length);
     }
     else if (y < 0) {
       const [row, col] = this.getRowCol();
@@ -153,7 +180,7 @@ export class Editor {
     }
     else if (y > 0) {
       const [row, col] = this.getRowCol();
-      const lines = this.codeBlock.text.split("\n");
+      const lines = block.text.split("\n");
       if (row < lines.length - 1) {
         this.setRowCol(row + 1, col);
       }
@@ -163,11 +190,12 @@ export class Editor {
 
   erase (offset) {
     const [before, after] = this.splitText();
+    const block = this.codeBlocks[this.tab];
     if (offset < 0) {
-      this.codeBlock.text = before.substring(0, before.length + offset) + after;
-      this.codeBlock.index = Math.max(0, this.codeBlock.index + offset);
+      block.text = before.substring(0, before.length + offset) + after;
+      block.index = Math.max(0, block.index + offset);
     } else {
-      this.codeBlock.text = before + after.substring(offset);
+      block.text = before + after.substring(offset);
     }
     this.update();
   }
@@ -175,7 +203,7 @@ export class Editor {
   execute () {
     const code = `import * as THREE from 'three';
                   export function run (scene) {
-                    ${this.codeBlock.text}
+                    ${this.codeBlocks[this.tab].text}
                   }
                  // ${Date.now()}`;
 
@@ -188,24 +216,15 @@ export class Editor {
     });
   }
 
-  getBlock () {
-    return this.codeBlocks.indexOf(this.codeBlock);
-  }
-
-  setBlock (block) {
-    this.codeBlock = this.codeBlocks[block];
-    this.update();
-  }
-
   nextBlock () {
-    const block = (this.getBlock() + 1) % this.codeBlocks.length;
-    this.setBlock(block);
+    this.tab = (this.tab + 1) % this.codeBlocks.length;
+    this.update();
   }
 
   previousBlock () {
     const len = this.codeBlocks.length;
-    const block = (this.getBlock() - 1 + len) % len;
-    this.setBlock(block);
+    this.tab = (this.tab - 1 + len) % this.codeBlocks.length;
+    this.update();
   }
 
   onKeyDown (event) {
@@ -213,11 +232,18 @@ export class Editor {
       if (event.key === "F") {
         this.execute();
       }
-      else if (event.key === "S") {
+      else if (event.key === "Tab") {
+        this.previousBlock();
+      }
+    }
+    else if (event.ctrlKey) {
+      if (event.key === "Tab") {
         this.nextBlock();
       }
-      else if (event.key === "X") {
-        this.previousBlock();
+      else if (event.key === "t") {
+        this.codeBlocks.push({text: "", index: 0});
+        this.tab = this.codeBlocks.length - 1;
+        this.update();
       }
     }
     else if (event.key === "Backspace") {
